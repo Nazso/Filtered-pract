4f00cbafda4da401515364356c8d496a7bbc2cfd
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-03 15:47:07 +0200

    server file

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/filtered_demo")
+// mongoose.connect("mongodb://localhost:27017/filtered_demo")
+mongoose.connect("mongodb+srv://Zsolt:ms3a3NztJiRKlVYn@cluster0.zq7ln.mongodb.net/filter_demo")
     .then(() => console.log('MongoDB connection is successful!'))
 
     
