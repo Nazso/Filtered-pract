cdd1c9c56c352014c3dd53e850e5033993f98135
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-03 15:00:51 +0200

    data for mongosh

---
+++ data.js
@@ -0,0 +1,556 @@
+db.viewers.insertMany(
+
+    [{
+        "id": 1,
+        "first_name": "Ashton",
+        "last_name": "Bowling",
+        "email": "abowling0@cam.ac.uk",
+        "movies": [
+          "Klown: The Movie (Klovn)",
+          "You've Got Mail",
+          "Major Payne"
+        ],
+        "address": {
+          "street": {
+            "name": "Ohio",
+            "number": "2",
+            "suffix": "Center"
+          },
+          "city": "Oranjestad",
+          "country": "Bonaire, Saint Eustatius and Saba "
+        },
+        "ratings": [
+          1,
+          3,
+          4
+        ],
+        "age": 83,
+        "status": "inactive"
+      }, {
+        "id": 2,
+        "first_name": "Olivier",
+        "last_name": "Gladtbach",
+        "email": "ogladtbach1@usa.gov",
+        "movies": [
+          "Blot, The",
+          "Exodus: Gods and Kings",
+          "Adventures of Sebastian Cole, The"
+        ],
+        "address": {
+          "street": {
+            "name": "Hazelcrest",
+            "number": "486",
+            "suffix": "Terrace"
+          },
+          "city": "Itupeva",
+          "country": "Brazil"
+        },
+        "ratings": [
+          4,
+          5,
+          4,
+          1
+        ],
+        "age": 90,
+        "status": "admin"
+      }, {
+        "id": 3,
+        "first_name": "Terese",
+        "last_name": "Tesmond",
+        "email": "ttesmond2@hp.com",
+        "movies": [
+          "Hangover Part II, The",
+          "Me and you (io e te)",
+          "Spriggan (Supurigan)"
+        ],
+        "address": {
+          "street": {
+            "name": "Menomonie",
+            "number": "4",
+            "suffix": "Parkway"
+          },
+          "city": "Solsona",
+          "country": "Philippines"
+        },
+        "ratings": [
+          3,
+          3,
+          5,
+          1,
+          1
+        ],
+        "age": 27,
+        "status": "active"
+      }, {
+        "id": 4,
+        "first_name": "Mavis",
+        "last_name": "Schwandermann",
+        "email": "mschwandermann3@wikimedia.org",
+        "movies": [
+          "Small Cuts (Petites coupures)",
+          "Kevin Smith: Burn in Hell",
+          "Fighting Prince of Donegal, The"
+        ],
+        "address": {
+          "street": {
+            "name": "Nancy",
+            "number": "3",
+            "suffix": "Road"
+          },
+          "city": "Shah Alam",
+          "country": "Malaysia"
+        },
+        "ratings": [
+          1,
+          3,
+          2
+        ],
+        "age": 30,
+        "status": "inactive"
+      }, {
+        "id": 5,
+        "first_name": "Abbott",
+        "last_name": "Disley",
+        "email": "adisley4@google.ca",
+        "movies": [
+          "Yamakasi - Les samouraïs des temps modernes",
+          "Turkish Delight (Turks fruit)",
+          "Women in Trouble"
+        ],
+        "address": {
+          "street": {
+            "name": "American",
+            "number": "641",
+            "suffix": "Plaza"
+          },
+          "city": "L'Aigle",
+          "country": "France"
+        },
+        "ratings": [
+          2,
+          3,
+          4,
+          2,
+          4
+        ],
+        "age": 64,
+        "status": "admin"
+      }, {
+        "id": 6,
+        "first_name": "Brien",
+        "last_name": "Dulake",
+        "email": "bdulake5@tripod.com",
+        "movies": [
+          "Wizard, The",
+          "I Hate Christian Laettner",
+          "Terrorist, The (a.k.a. Malli) (Theeviravaathi)"
+        ],
+        "address": {
+          "street": {
+            "name": "Bunker Hill",
+            "number": "5",
+            "suffix": "Place"
+          },
+          "city": "Saint-Priest",
+          "country": "France"
+        },
+        "ratings": [
+          2,
+          2,
+          3,
+          5,
+          1
+        ],
+        "age": 20,
+        "status": "admin"
+      }, {
+        "id": 7,
+        "first_name": "Antoine",
+        "last_name": "Gianettini",
+        "email": "agianettini6@acquirethisname.com",
+        "movies": [
+          "Educating Rita",
+          "Devil's Diary",
+          "First Power, The"
+        ],
+        "address": {
+          "street": {
+            "name": "Golden Leaf",
+            "number": "3",
+            "suffix": "Junction"
+          },
+          "city": "Katwijk",
+          "country": "Netherlands"
+        },
+        "ratings": [
+          1,
+          5,
+          2,
+          5,
+          4,
+          2
+        ],
+        "age": 77,
+        "status": "admin"
+      }, {
+        "id": 8,
+        "first_name": "Jeane",
+        "last_name": "Lortzing",
+        "email": "jlortzing7@symantec.com",
+        "movies": [
+          "That's What I Am",
+          "Daniel",
+          "Capture of Bigfoot, The"
+        ],
+        "address": {
+          "street": {
+            "name": "Kinsman",
+            "number": "42",
+            "suffix": "Terrace"
+          },
+          "city": "Villazón",
+          "country": "Bolivia"
+        },
+        "ratings": [
+          3,
+          3,
+          4
+        ],
+        "age": 47,
+        "status": "active"
+      }, {
+        "id": 9,
+        "first_name": "Nahum",
+        "last_name": "Ralls",
+        "email": "nralls8@webmd.com",
+        "movies": [
+          "Velvet Vampire, The",
+          "Reel Bad Arabs: How Hollywood Vilifies a People",
+          "My Father and My Son (Babam ve oglum)"
+        ],
+        "address": {
+          "street": {
+            "name": "Summerview",
+            "number": "6",
+            "suffix": "Point"
+          },
+          "city": "Finiq",
+          "country": "Albania"
+        },
+        "ratings": [
+          5,
+          5,
+          3,
+          2,
+          4,
+          1
+        ],
+        "age": 81,
+        "status": "inactive"
+      }, {
+        "id": 10,
+        "first_name": "Alick",
+        "last_name": "Nelissen",
+        "email": "anelissen9@bandcamp.com",
+        "movies": [
+          "Family Meeting",
+          "Wild Bunch, The",
+          "Seed of Chucky (Child's Play 5)"
+        ],
+        "address": {
+          "street": {
+            "name": "Crownhardt",
+            "number": "970",
+            "suffix": "Way"
+          },
+          "city": "Bąków",
+          "country": "Poland"
+        },
+        "ratings": [
+          5,
+          3,
+          1
+        ],
+        "age": 57,
+        "status": "active"
+      }, {
+        "id": 11,
+        "first_name": "Janith",
+        "last_name": "Dameisele",
+        "email": "jdameiselea@sun.com",
+        "movies": [
+          "Hick",
+          "Suomisen Olli yllättää",
+          "Blackmailed"
+        ],
+        "address": {
+          "street": {
+            "name": "Sheridan",
+            "number": "68",
+            "suffix": "Junction"
+          },
+          "city": "San Matías",
+          "country": "Bolivia"
+        },
+        "ratings": [
+          5,
+          2,
+          1
+        ],
+        "age": 60,
+        "status": "inactive"
+      }, {
+        "id": 12,
+        "first_name": "Manon",
+        "last_name": "Dredge",
+        "email": "mdredgeb@baidu.com",
+        "movies": [
+          "Beware of Mr. Baker",
+          "Rookie, The",
+          "Live!"
+        ],
+        "address": {
+          "street": {
+            "name": "Welch",
+            "number": "7529",
+            "suffix": "Crossing"
+          },
+          "city": "Banjarwaru",
+          "country": "Indonesia"
+        },
+        "ratings": [
+          1,
+          2,
+          5,
+          3,
+          1,
+          2
+        ],
+        "age": 64,
+        "status": "admin"
+      }, {
+        "id": 13,
+        "first_name": "Antonia",
+        "last_name": "Sunter",
+        "email": "asunterc@disqus.com",
+        "movies": [
+          "Marriage Italian Style (Matrimonio all'italiana)",
+          "High Tension (Haute tension) (Switchblade Romance)",
+          "Killers, The"
+        ],
+        "address": {
+          "street": {
+            "name": "Messerschmidt",
+            "number": "5",
+            "suffix": "Park"
+          },
+          "city": "Gisiliba",
+          "country": "Indonesia"
+        },
+        "ratings": [
+          3,
+          4,
+          5,
+          3,
+          4
+        ],
+        "age": 33,
+        "status": "admin"
+      }, {
+        "id": 14,
+        "first_name": "Langsdon",
+        "last_name": "Bickle",
+        "email": "lbickled@china.com.cn",
+        "movies": [
+          "Red Beret, The",
+          "Beau Brummel",
+          "Off the Charts: The Song-Poem Story"
+        ],
+        "address": {
+          "street": {
+            "name": "Roxbury",
+            "number": "4898",
+            "suffix": "Center"
+          },
+          "city": "Houping",
+          "country": "China"
+        },
+        "ratings": [
+          1,
+          3,
+          2,
+          4,
+          2
+        ],
+        "age": 86,
+        "status": "admin"
+      }, {
+        "id": 15,
+        "first_name": "Tito",
+        "last_name": "Wheildon",
+        "email": "twheildone@cafepress.com",
+        "movies": [
+          "New Daughter, The",
+          "Wild Oranges",
+          "Hacks (Sink or Swim) (Big Twist, The)"
+        ],
+        "address": {
+          "street": {
+            "name": "Montana",
+            "number": "556",
+            "suffix": "Circle"
+          },
+          "city": "Miaofengshan",
+          "country": "China"
+        },
+        "ratings": [
+          2,
+          5,
+          2,
+          5,
+          3,
+          2
+        ],
+        "age": 87,
+        "status": "inactive"
+      }, {
+        "id": 16,
+        "first_name": "Rusty",
+        "last_name": "Niche",
+        "email": "rnichef@php.net",
+        "movies": [
+          "Young Americans, The",
+          "Nightmare Alley",
+          "The Big Bus"
+        ],
+        "address": {
+          "street": {
+            "name": "6th",
+            "number": "9",
+            "suffix": "Pass"
+          },
+          "city": "Piduhe",
+          "country": "China"
+        },
+        "ratings": [
+          3,
+          3,
+          3,
+          2,
+          5,
+          5
+        ],
+        "age": 22,
+        "status": "admin"
+      }, {
+        "id": 17,
+        "first_name": "Stanwood",
+        "last_name": "Freegard",
+        "email": "sfreegardg@hexun.com",
+        "movies": [
+          "Gordy",
+          "Arabian Nights",
+          "Escape Artist, The"
+        ],
+        "address": {
+          "street": {
+            "name": "Cody",
+            "number": "465",
+            "suffix": "Road"
+          },
+          "city": "Kopychyntsi",
+          "country": "Ukraine"
+        },
+        "ratings": [
+          2,
+          2,
+          4,
+          5
+        ],
+        "age": 34,
+        "status": "active"
+      }, {
+        "id": 18,
+        "first_name": "Hermia",
+        "last_name": "Geraud",
+        "email": "hgeraudh@skype.com",
+        "movies": [
+          "Ivan's Childhood (a.k.a. My Name is Ivan) (Ivanovo detstvo)",
+          "Gore Vidal: The United States of Amnesia",
+          "Moving McAllister"
+        ],
+        "address": {
+          "street": {
+            "name": "Elmside",
+            "number": "52",
+            "suffix": "Point"
+          },
+          "city": "Wuyahe",
+          "country": "China"
+        },
+        "ratings": [
+          1,
+          2,
+          5,
+          1,
+          1,
+          3
+        ],
+        "age": 73,
+        "status": "active"
+      }, {
+        "id": 19,
+        "first_name": "Filbert",
+        "last_name": "Lerwill",
+        "email": "flerwilli@zdnet.com",
+        "movies": [
+          "Breakfast Club, The",
+          "Chicago",
+          "Tales from the Golden Age (Amintiri din epoca de aur)"
+        ],
+        "address": {
+          "street": {
+            "name": "Killdeer",
+            "number": "25",
+            "suffix": "Terrace"
+          },
+          "city": "Lekas",
+          "country": "Albania"
+        },
+        "ratings": [
+          4,
+          1,
+          1
+        ],
+        "age": 19,
+        "status": "admin"
+      }, {
+        "id": 20,
+        "first_name": "Gerianna",
+        "last_name": "Hedon",
+        "email": "ghedonj@discovery.com",
+        "movies": [
+          "Flame and the Arrow, The",
+          "White Sheik, The (Sceicco bianco, Lo)",
+          "Arbor, The"
+        ],
+        "address": {
+          "street": {
+            "name": "Utah",
+            "number": "69",
+            "suffix": "Crossing"
+          },
+          "city": "Baki",
+          "country": "Indonesia"
+        },
+        "ratings": [
+          4,
+          1,
+          3,
+          4,
+          5
+        ],
+        "age": 74,
+        "status": "active"
+      }]
+)
