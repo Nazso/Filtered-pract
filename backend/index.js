a46ad5e7c0381fcd8bee56bb03160bd704deb56b
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-03 17:08:43 +0200

    server file

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,18 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://Zsolt:ms3a3NztJiRKlVYn@cluster0.zq7ln.mongodb.net/filter_demo")
     .then(() => console.log('MongoDB connection is successful!'))
 
+const All = mongoose.model('User', new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String
+}))
+
 const app = express();
+
+app.get('/user', async (req, res) => {
+    const peopleAll = await All.find()
+    res.json(peopleAll);
+})
 
 
 app.listen(3000, () => {
