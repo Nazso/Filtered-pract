cc240f275ea3b4c0988414db27da6a15be63478c
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-03 16:09:16 +0200

    server file

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
+const express = require('express');
 const mongoose = require('mongoose');
 
 // mongoose.connect("mongodb://localhost:27017/filtered_demo")
 mongoose.connect("mongodb+srv://Zsolt:ms3a3NztJiRKlVYn@cluster0.zq7ln.mongodb.net/filter_demo")
     .then(() => console.log('MongoDB connection is successful!'))
 
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log('App is listening on port: 3000!')
+})
     
