b7ef40543054f7b9411a8873640fa60ef9a07fe9
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-04 11:37:04 +0200

    server file

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const proxy = require('express-http-proxy');
 
 // mongoose.connect("mongodb://localhost:27017/filtered_demo")
 mongoose.connect("mongodb+srv://Zsolt:ms3a3NztJiRKlVYn@cluster0.zq7ln.mongodb.net/filter_demo")
@@ -14,10 +15,22 @@ const All = mongoose.model('User', new mongoose.Schema({
 const app = express();
 
 app.get('/user', async (req, res) => {
-    const peopleAll = await All.find()
+
+    const filter = {}
+
+    if(req.query.firstName) {
+        filter.firstName = new RegExp('^' + req.query.firstName)
+    }
+
+    if(req.query.lastName) {
+        filter.lastName = new RegExp('^' + req.query.lastName)
+    }
+
+    const peopleAll = await All.find(filter).sort('firstName')
     res.json(peopleAll);
 })
 
+app.use('/', proxy('localhost:4200'));
 
 app.listen(3000, () => {
     console.log('App is listening on port: 3000!')
