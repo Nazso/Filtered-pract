2b148da8a3e09a1431fa58f2c2d2e8cbba825a89
Nazso <zsolt.nagy.1976@gmail.com>
2023-07-04 11:37:29 +0200

    logic

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { UserModel } from './user.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Frontend';
+export class AppComponent implements OnInit {
+
+  users?: Observable<UserModel[]>;
+  filter = {
+    firstName: '',
+    lastName: ''
+  }
+  
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.loadUsers()
+  }
+  
+  loadUsers() {
+    this.users = this.userService.getUser(this.filter);
+  }
+
+
 }
